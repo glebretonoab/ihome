bc4f9fd57de8d6271c0c1d157bcdeb875b819a71
Gaetan LEBRETON <lebreton.gaetan@gmail.com>
2017-11-28 12:34:46 +0000

    Change url server

--- build/dev.js
+++ build/dev.js
@@ -2,8 +2,14 @@ var webpack = require('webpack')
 var config = require('../config/webpack.dev.js')
 var webpackDevServer = require('webpack-dev-server')
 var port = process.env.PORT || 8080;
+var server_url = 'http://localhost:' + port + '/';
+
+if (process.env.HEROKU_APP_NAME) {
+    server_url = "https://" + process.env.HEROKU_APP_NAME + ".herokuapp.com";
+}
+
 config.entry.app.unshift(
-  "webpack-dev-server/client?http://localhost:" + port + "/",
+  "webpack-dev-server/client?" + server_url,
   "webpack/hot/dev-server"
 )
 
